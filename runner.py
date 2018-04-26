ddec8dfc0895ace41a2f4aeff5d4504eb4c8e5a7
Hirad Emami Alagha <Hirad.alagha@outlook.com>
2018-04-26 15:29:58 +0200

    new visualization

--- runner.py
+++ runner.py
@@ -1,36 +1,44 @@
 import world as wd
 from worldObject import *
 import agent
-import visualization
+import visualization_tk
+from tkinter import *
 
 
-obstacles =[]
-obstacle1 = obstacle("wall",argWidth=2, argHight=2, argX=5,argY=5, argId=-1)
-obstacle2 = obstacle("swamp",3,3,10,10,-2)
+initial=True
+if initial:
+    obstacles = []
+    obstacle1 = obstacle("wall", argWidth=2, argHight=2, argX=5, argY=5, argId=-1)
+    obstacle2 = obstacle("swamp", 3, 3, 10, 10, -2)
 
-obstacles.append(obstacle1)
-obstacles.append(obstacle2)
+    obstacles.append(obstacle1)
+    obstacles.append(obstacle2)
 
-goals=[]
-goa11= goal("red",argWidth=2, argHight=3, argX=3,argY=3, argId=100)
-goa12= goal("blue",argWidth=2, argHight=2, argX=17,argY=11, argId=101)
+    goals = []
+    goa11 = goal("red", argWidth=2, argHight=3, argX=3, argY=3, argId=100)
+    goa12 = goal("blue", argWidth=2, argHight=2, argX=17, argY=11, argId=101)
 
-goals.append(goa11)
-goals.append(goa12)
+    goals.append(goa11)
+    goals.append(goa12)
 
-agents=[]
-agen1=agent.agent(argId=1)
-agen2=agent.agent(argId=2)
+    agents = []
+    agen1 = agent.agent(argId=1)
+    agen2 = agent.agent(argId=2)
 
-agents.append(agen1)
-agents.append(agen2)
+    agents.append(agen1)
+    agents.append(agen2)
 
-world = wd.world("Create")
-world.createWorld(argWidth=30,argHeigth=20, argObstacleInfo=obstacles, argAgentInfo=agents, goalInfo=goals)
-print(world.board)
+    world = wd.world("Create")
+    world.createWorld(argWidth=30, argHeigth=20, argObstacleInfo=obstacles, argAgentInfo=agents, goalInfo=goals)
+    print(world.board)
 
-viz = visualization.visualization(world)
-viz.display_simulation()
+master=Tk()
+viz = visualization_tk.visualization(world, master)
+print("hello")
+viz.frame.grid(row=0, column=0)
+
+
+
 
 
 
--- visualization.py
+++ visualization.py
@@ -1,113 +1,37 @@
-__author__='Hirad Emami Alagha - S3218139'
+__author__='Hirad Emami Alagha'
 
-#required Libraries and Classes
-import world
-import tkinter as tk
-from tkinter import *
-
-
-##visualization class that provides graphical representation of the 2D grid
-##including the obsticles, agents and the goal
-class visualization(tk.Tk):
-    ##primary Root of visualization in tkinter is passed by argRoot
-    def __init__(self,argWorld):
-        #cell scaling value given the size of the world
-        #the size of the window will be static 600x600 for square worlds
-        #the size of the window will be either 700x400 or 700x400 for worlds that are rectangle
-        self.world=argWorld
-        self.frame_widthScale, self.frame_heightScale = self.calculateTheScale(height=self.world.height, width=self.world.width)
-        #the root of tkinter
-        self.master = tk.Tk()
-        #name and size of the window
-        # name and size of the window
-        self.master.title("MARL - Simulation Display")
-        print(self.world.width * self.frame_widthScale, self.world.height * self.frame_heightScale)
-        #the main frame initialization and setting the size of the window based on the scale and the size of the world
-        self.frame = Canvas(self.master, height=(self.world.height * self.frame_heightScale),
-                            width=(self.world.width * self.frame_widthScale))
-
-        self.render()
-
-    def calculateTheScale(self,height, width):
-        # if the world is square
-        if (height == width):
-            return 600 / width, 600 / height
-        # if the world's height is bigger than the width
-        elif (height > width):
-            return 400 / width, 700 / height
-        # if the world's width is bigger than the hight
-        else:
-            return 700 / width, 400 / height
-
-    #Rendering THe Grid
-    def render(self):
-        for i in range(self.world.height):
-            for j in range(self.world.width):
-                type=self.set_type(self.world.board[i][j])
-                self.draw_cell(type,i,j)
-
-    def draw_cell(self,argType,i,j):
-        color=self.set_color(argType)
-        self.frame.create_rectangle(j * self.frame_widthScale, i * self.frame_heightScale,
-                                    (j + 1) * self.frame_widthScale, (i + 1) * self.frame_heightScale,
-                                    fill=color, width=1, outline="gray1")
-
-    def set_color(self,argType):
-        colors = {
-            "agent": "light sky blue",
-            "obstacle": "black",
-            "goal": "green",
-            "empty": "white"
-        }
-        return colors.get(argType,"white")
-
-    def set_type(self,argValue):
-        if argValue<0:
-            return "obstacle"
-        elif argValue == 0:
-            return "empty"
-        elif argValue>0 and argValue<100:
-            return "agent"
-        elif argValue > 99:
-            return "goal"
-
-    def display_simulation(self):
-        self.frame.grid(row=0, column=0)
-        self.master.update()
-        self.draw_cell("goal", 2, 1)
-
-    def createWidgets(self):
-        self.QUIT = Button(self)
-        self.QUIT["text"] = "QUIT"
-        self.QUIT["fg"]   = "red"
-        self.QUIT["command"] =  self.quit
-
-        self.QUIT.pack({"side": "left"})
-
-        self.hi_there = Button(self)
-        self.hi_there["text"] = "Hello",
-        self.hi_there["command"] = self.say_hi
-
-        self.hi_there.pack({"side": "left"})
+import numpy as np
+import matplotlib.ticker as plticker
+import world as wd
+import agent
+from worldObject import *
+from matplotlib import pyplot as plt
+from matplotlib import colors
 
 
-if __name__ == '__main__':
-    print("__________EXAMPLE WORLD_________")
-    width = 5
-    height = 10
-    numObsticles = 2
-    obsticleInfo = []
 
-    for i in range(numObsticles):
-        obsticleInfo.append(('wall', 1, 2))
+if __name__ =='__main__':
+    obstacles = []
+    obstacle1 = obstacle("wall", argWidth=2, argHight=2, argX=5, argY=5, argId=-1)
+    obstacle2 = obstacle("swamp", 3, 3, 10, 10, -2)
 
-    print("your sample wolrld is created as:")
-    world = world.world(width, height, numObsticles, obsticleInfo)
+    obstacles.append(obstacle1)
+    obstacles.append(obstacle2)
+
+    goals = []
+    goa11 = goal("red", argWidth=2, argHight=3, argX=3, argY=3, argId=100)
+    goa12 = goal("blue", argWidth=2, argHight=2, argX=17, argY=11, argId=101)
+
+    goals.append(goa11)
+    goals.append(goa12)
+
+    agents = []
+    agen1 = agent.agent(argId=1)
+    agen2 = agent.agent(argId=2)
+
+    agents.append(agen1)
+    agents.append(agen2)
+
+    world = wd.world("Create")
+    world.createWorld(argWidth=20, argHeigth=20, argObstacleInfo=obstacles, argAgentInfo=agents, goalInfo=goals)
     print(world.board)
-    print(world.obsticle_info)
-    print("____________________________")
-
-    root = Tk()
-    app = visualization(world, argRoot=root)
-    app.mainloop(root)
-    root.destroy()
